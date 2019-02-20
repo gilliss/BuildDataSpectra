bad3e3c1fdcc15828f17183073a8604284ece100
Tom Gilliss <gilliss@unc.edu>
2019-02-20 18:42:31 -0500

    moved env vars back into settings

--- Management/Settings.py
+++ Management/Settings.py
@@ -10,3 +10,8 @@ class Settings:
         self.data_set = args.data_set
         self.data_type = args.data_type
         self.file_index = args.file_index
+
+        # Environment variables
+        self.skim_data_dirname = os.environ['MJDDATADIR'] + '/surfmjd/analysis/skim'
+        self.channel_data_dirname = os.environ['CHANNELDATADIR']
+        self.bds_output_dirname = os.environ['BDSOUTPUTDIR']
